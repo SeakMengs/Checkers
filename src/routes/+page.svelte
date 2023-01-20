a3f57c2d2b6b20f7fe6e4b0ed7eebf9cc9bf2b3a
Yato <54373229+SeakMengs@users.noreply.github.com>
2023-01-21 02:49:45 +0700

    create board

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,70 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- 
+	@Author: Seakmeng Hor
+	@Date: 1/21/2023
+ -->
+
+<!-- Script goes here -->
+<script>
+    // image source
+    let src = {
+        cat: 'Logo.jpg',
+        dog: 'favicon.jpg'
+    }
+
+    // give default pieces position (discrete math usage: state machine)
+    let pieces = [
+        [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [2, 0, 2, 0, 2, 0, 2, 0, 2, 0],
+        [0, 2, 0, 2, 0, 2, 0, 2, 0, 2],
+        [2, 0, 2, 0, 2, 0, 2, 0, 2, 0],
+    ];
+    // set default 1dx8 board
+    const boardN = 10;
+    const boardM = 8;
+
+</script>
+
+<!-- Script end here -->
+
+<!-- HTML goes here -->
+<div class="container">
+    <div class="board">
+        {#each pieces as arrPiece}
+            {#each arrPiece as piece, i }
+                {#if piece == 1 || piece == 2}
+                    <img class="" src={src.cat} alt="cat">
+                {:else}
+                    <img src={src.dog} alt="dog">
+                {/if}
+            {/each}
+        {/each}
+    </div>
+</div>
+<!-- HTML end here -->
+
+
+<!-- Style end here  -->
+
+<!-- Style goes here  -->
+<style>
+    .container {
+        width: 90%;
+        height: 100vh;
+        margin: 0 auto;
+    }
+
+    .board {
+        display: grid;
+        /* make grid max 10 */
+        grid-template-columns: repeat(10, 50px);
+    }
+
+    img {
+        width: 50px;
+        height: auto;
+    }
+</style>
