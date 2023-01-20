c68e5665343900a1119828010a3d3c5103ce5d66
Yato <54373229+SeakMengs@users.noreply.github.com>
2023-01-21 03:10:27 +0700

    switch port and add css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,17 +32,19 @@
 
 <!-- HTML goes here -->
 <div class="container">
-    <div class="board">
-        {#each pieces as arrPiece}
-            {#each arrPiece as piece, i }
-                {#if piece == 1 || piece == 2}
-                    <img class="" src={src.cat} alt="cat">
-                {:else}
-                    <img src={src.dog} alt="dog">
-                {/if}
+        <div class="board">
+            {#each pieces as arrPiece}
+                {#each arrPiece as piece, i }
+                    {#if piece == 1 || piece == 2}
+                        <img class="piece-board piece-{piece}" src={src.cat} alt="cat">
+                        <!-- <div class="piece-board piece-{piece}"></div> -->
+                    {:else}
+                        <img class="piece-board piece-{piece}" src={src.dog} alt="dog">
+                        <!-- <div class="piece-board piece-{piece}"></div> -->
+                    {/if}
+                {/each}
             {/each}
-        {/each}
-    </div>
+        </div>
 </div>
 <!-- HTML end here -->
 
@@ -52,15 +54,33 @@
 <!-- Style goes here  -->
 <style>
     .container {
-        width: 90%;
+        width: 80%;
         height: 100vh;
         margin: 0 auto;
     }
 
     .board {
+        width: 50%;
         display: grid;
         /* make grid max 10 */
         grid-template-columns: repeat(10, 50px);
+        justify-content: center;
+    }
+
+    .piece-board {
+        border: 1px solid greenyellow;
+    }
+
+    .piece-0 {
+        width: 50px;
+        height: 50px;
+        background: rgb(77, 56, 56);
+    }
+
+    .piece-2, .piece-1 {
+        width: 50px;
+        height: 50px;
+        background: wheat;
     }
 
     img {
