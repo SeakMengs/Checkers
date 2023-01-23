6ce64ae8a1ad2b075cc443834e507bd3bfd1392c
Yato <54373229+SeakMengs@users.noreply.github.com>
2023-01-23 23:28:44 +0700

    add select piece

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,8 @@
+<!-- 
+    @Author: Seakmeng Hor
+    @Date: 1/21/2023
+ -->
+
 <!-- Script goes here -->
 <script>
     // image source
@@ -23,14 +28,14 @@
         [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
         [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
         [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [2, 0, 2, 0, 2, 0, 2, 0, 2, 0],
         [0, 2, 0, 2, 0, 2, 0, 2, 0, 2],
         [2, 0, 2, 0, 2, 0, 2, 0, 2, 0],
     ];
 
-    // reset board to default
+    // reset pieces to default
     function resetGame() {
         pieces = [
             [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
@@ -42,40 +47,54 @@
             [0, 2, 0, 2, 0, 2, 0, 2, 0, 2],
             [2, 0, 2, 0, 2, 0, 2, 0, 2, 0],
         ];
+
+        // remove selected class from all pieces
+        document.querySelectorAll(".pieces").forEach((piece) => {
+        piece.classList.remove("selected");
+        });
+    }
+
+    let player1Turn = false;
+
+
+    /**
+     * @param {any} event
+     */
+    function selectPiece(event) {
+        // remove selected class from all pieces
+        document.querySelectorAll(".pieces").forEach((piece) => {
+            piece.classList.remove("selected");
+        });
+        
+        event.target.classList.toggle("selected");
     }
 </script>
-
-<!-- 
-	@Author: Seakmeng Hor
-	@Date: 1/21/2023
- -->
-
-
 <!-- Script end here -->
 
 <!-- HTML goes here -->
 <div class="container">
-    <!-- <div class="board">
-        {#each pieces as arrPiece, j}
-        <div class="board-{j} board-row">
-            {#each arrPiece as piece, i }
-                {#if piece == 1 || piece == 2}
-                    <img class="piece-board piece-{piece}" src={src.cat} alt="cat">
-                    <div class="piece-board piece-{piece}"></div>
-                {:else}
-                    <img class="piece-board piece-{piece}" src={src.dog} alt="dog">
-                    <div class="piece-board piece-{piece}"></div>
-                {/if}
-            {/each}
-        </div>
-        {/each}
-    </div> -->
+    <div class="Content">
+        <span>Player {player1Turn == true ? "one" : "two"}'s turn</span>
+    </div>
+    <!-- render board and piece -->
     <div class="board">
         {#each BOARD as row, i}
             {#each row as col, j}
-            <div class="cell" class:red={BOARD[i][j]}>
-                    
-            </div>
+                <div class="cell m{i} n{j}" class:dark={BOARD[i][j]}>
+                    {#if pieces[i][j] == 1 || pieces[i][j] == 3}
+                        <button
+                            class="pieces piece-{i}-{j} player-1"
+                            class:king1={pieces[i][j] == 3}
+                            on:click={selectPiece}
+                        />
+                    {:else if pieces[i][j] == 2 || pieces[i][j] == 4}
+                        <button
+                            class="pieces piece-{i}-{j} player-2"
+                            class:king2={pieces[i][j] == 4}
+                            on:click={selectPiece}
+                        />
+                    {/if}
+                </div>
             {/each}
         {/each}
     </div>
@@ -83,27 +102,53 @@
 </div>
 <!-- HTML end here -->
 
+
 <!-- Style goes here  -->
 <style>
     .container {
         width: 100%;
         height: 100vh;
+        margin: 0 auto;
         display: grid;
         place-items: center;
     }
+    
     .board {
         display: grid;
         grid-template-columns: repeat(10, 5rem);
-        border: 1px solid white;
+        border-radius: 0.5rem solid white;
+        box-shadow: 0 0 10px 5px #ffffff;
     }
-
+    
     .cell {
         width: 5rem;
         height: 5rem;
+        background-color: #ee1b24;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .dark {
         background-color: #251e21;
     }
-    .red {
-        background-color: #ee1b24;
+    
+    .pieces {
+        width: 4rem;
+        height: 4rem;
+        border-radius: 100%;
+        border: none;
     }
-</style>
+    
+    .player-1 {
+        background-color: rgb(164, 164, 192);
+    }
+    
+    .player-2 {
+        background-color: rgb(192, 164, 164);
+    }
+    
+    .selected {
+        box-shadow: 0 0 10px 5px #ffffff;
+    }
+    </style>
 <!-- Style end here  -->
