62fef9223d5795c8775c90e00ec1e26e3abdf040
Yato <54373229+SeakMengs@users.noreply.github.com>
2023-01-23 20:05:43 +0700

    rewrite css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,91 +1,109 @@
-<!-- 
-	@Author: Seakmeng Hor
-	@Date: 1/21/2023
- -->
-
 <!-- Script goes here -->
 <script>
     // image source
     let src = {
-        cat: 'Logo.jpg',
-        dog: 'favicon.jpg'
-    }
+        cat: "Logo.jpg",
+        dog: "favicon.jpg",
+    };
+
+    const BOARD = [
+        [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+    ];
 
     // give default pieces position (discrete math usage: state machine)
+    // 1 = player 1, 2 = player 2, 0 = empty, 3 = player 1 king, 4 = player 2 king
     let pieces = [
         [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
         [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
         [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [2, 0, 2, 0, 2, 0, 2, 0, 2, 0],
         [0, 2, 0, 2, 0, 2, 0, 2, 0, 2],
         [2, 0, 2, 0, 2, 0, 2, 0, 2, 0],
     ];
-    // set default 1dx8 board
-    const boardN = 10;
-    const boardM = 8;
 
+    // reset board to default
+    function resetGame() {
+        pieces = [
+            [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+            [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+            [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [2, 0, 2, 0, 2, 0, 2, 0, 2, 0],
+            [0, 2, 0, 2, 0, 2, 0, 2, 0, 2],
+            [2, 0, 2, 0, 2, 0, 2, 0, 2, 0],
+        ];
+    }
 </script>
+
+<!-- 
+	@Author: Seakmeng Hor
+	@Date: 1/21/2023
+ -->
+
 
 <!-- Script end here -->
 
 <!-- HTML goes here -->
 <div class="container">
-        <div class="board">
-            {#each pieces as arrPiece, j}
-            <div class="board-{j} board-row">
-                {#each arrPiece as piece, i }
-                    {#if piece == 1 || piece == 2}
-                        <img class="piece-board piece-{piece}" src={src.cat} alt="cat">
-                        <!-- <div class="piece-board piece-{piece}"></div> -->
-                    {:else}
-                        <img class="piece-board piece-{piece}" src={src.dog} alt="dog">
-                        <!-- <div class="piece-board piece-{piece}"></div> -->
-                    {/if}
-                {/each}
-            </div>
+    <!-- <div class="board">
+        {#each pieces as arrPiece, j}
+        <div class="board-{j} board-row">
+            {#each arrPiece as piece, i }
+                {#if piece == 1 || piece == 2}
+                    <img class="piece-board piece-{piece}" src={src.cat} alt="cat">
+                    <div class="piece-board piece-{piece}"></div>
+                {:else}
+                    <img class="piece-board piece-{piece}" src={src.dog} alt="dog">
+                    <div class="piece-board piece-{piece}"></div>
+                {/if}
             {/each}
         </div>
+        {/each}
+    </div> -->
+    <div class="board">
+        {#each BOARD as row, i}
+            {#each row as col, j}
+            <div class="cell" class:red={BOARD[i][j]}>
+                    
+            </div>
+            {/each}
+        {/each}
+    </div>
+    <button on:click={resetGame}>Restart the game</button>
 </div>
 <!-- HTML end here -->
-
-
-<!-- Style end here  -->
 
 <!-- Style goes here  -->
 <style>
     .container {
         width: 100%;
         height: 100vh;
-        margin: 0 auto;
+        display: grid;
+        place-items: center;
     }
-
     .board {
-        display: flex;
-        flex-direction: column;
+        display: grid;
+        grid-template-columns: repeat(10, 5rem);
+        border: 1px solid white;
     }
 
-    .board-row {
-        display: flex;
-        justify-content: center;
-        align-items: center;
+    .cell {
+        width: 5rem;
+        height: 5rem;
+        background-color: #251e21;
     }
-
-    .piece-board {
-        border: 1px solid greenyellow;
+    .red {
+        background-color: #ee1b24;
     }
-
-    .piece-0 {
-        width: 5%;
-        height: auto;
-        background: rgb(77, 56, 56);
-    }
-
-    .piece-2, .piece-1 {
-        width: 5%;
-        height: auto;
-        background: wheat;
-    }
-
 </style>
+<!-- Style end here  -->
