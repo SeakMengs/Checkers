92b108ae0ffab62d27bc4f07df0604e466ed43f6
Yato <54373229+SeakMengs@users.noreply.github.com>
2023-01-28 03:34:40 +0700

    finish all logic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,28 +23,30 @@
 
     // give default pieces position (discrete math usage: state machine)
     // 1 = player 1, 2 = player 2, 0 = empty, 3 = player 1 king, 4 = player 2 king
-    // let pieces = [
-    //     [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    //     [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-    //     [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    //     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    //     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    //     [2, 0, 2, 0, 2, 0, 2, 0, 2, 0],
-    //     [0, 2, 0, 2, 0, 2, 0, 2, 0, 2],
-    //     [2, 0, 2, 0, 2, 0, 2, 0, 2, 0],
-    // ];
     let pieces = [
         [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
         [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
         [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-        [0, 0, 2, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [2, 0, 2, 0, 2, 0, 2, 0, 2, 0],
         [0, 2, 0, 2, 0, 2, 0, 2, 0, 2],
         [2, 0, 2, 0, 2, 0, 2, 0, 2, 0],
     ];
 
-    checkPlayerScore()
+    // set up win
+    // let pieces = [
+    //     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    //     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    //     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    //     [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+    //     [0, 0, 0, 2, 0, 0, 0, 0, 0, 0],
+    //     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    //     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    //     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    // ];
+
+    checkPlayerScore();
 
     // reset pieces to default
     function resetGame() {
@@ -69,7 +71,17 @@
 
         // reset player turn
         player1Turn = true;
-        checkPlayerScore()
+        checkPlayerScore();
+    }
+
+    function checkPlayerWon() {
+        if (playerOneScore == 0) {
+            alert("Player 2 Won!");
+            resetGame();
+        } else if (playerTwoScore == 0) {
+            alert("Player 1 Won!");
+            resetGame();
+        }
     }
 
     // switch player turn
@@ -78,23 +90,25 @@
     }
 
     function checkPlayerScore() {
-        playerOneScore = 0
-        playerTwoScore = 0
+        playerOneScore = 0;
+        playerTwoScore = 0;
 
         for (let i = 0; i < pieces.length; i++) {
             for (let j = 0; j < pieces[i].length; j++) {
                 if (pieces[i][j] == 1 || pieces[i][j] == 3) {
-                    playerOneScore++
+                    playerOneScore++;
                 } else if (pieces[i][j] == 2 || pieces[i][j] == 4) {
-                    playerTwoScore++
+                    playerTwoScore++;
                 }
             }
         }
+
+        checkPlayerWon();
     }
 
     // remove all possible move class
     function clearPossibleMove() {
-        document.querySelectorAll('.possible-move').forEach((piece) => {
+        document.querySelectorAll(".possible-move").forEach((piece) => {
             // change child class from hiddenMovable to movable
             piece.children[0].classList.remove("movable");
             piece.children[0].classList.add("hiddenMovable");
@@ -157,14 +171,22 @@
         let index = element.classList[1].split("-");
         let newElement = null;
 
-        if (move == 'goDownLeft') {
-            newElement = document.querySelectorAll(`.mn-${parseInt(index[1]) + 1}-${parseInt(index[2]) - 1}`);
-        } else if (move == 'goDownRight') {
-            newElement = document.querySelectorAll(`.mn-${parseInt(index[1]) + 1}-${parseInt(index[2]) + 1}`);
-        } else if (move == 'goUpLeft') {
-            newElement = document.querySelectorAll(`.mn-${parseInt(index[1]) - 1}-${parseInt(index[2]) - 1}`);
-        } else if (move == 'goUpRight') {
-            newElement = document.querySelectorAll(`.mn-${parseInt(index[1]) - 1}-${parseInt(index[2]) + 1}`);
+        if (move == "goDownLeft") {
+            newElement = document.querySelectorAll(
+                `.mn-${parseInt(index[1]) + 1}-${parseInt(index[2]) - 1}`
+            );
+        } else if (move == "goDownRight") {
+            newElement = document.querySelectorAll(
+                `.mn-${parseInt(index[1]) + 1}-${parseInt(index[2]) + 1}`
+            );
+        } else if (move == "goUpLeft") {
+            newElement = document.querySelectorAll(
+                `.mn-${parseInt(index[1]) - 1}-${parseInt(index[2]) - 1}`
+            );
+        } else if (move == "goUpRight") {
+            newElement = document.querySelectorAll(
+                `.mn-${parseInt(index[1]) - 1}-${parseInt(index[2]) + 1}`
+            );
         }
 
         if (newElement == null) {
@@ -186,7 +208,7 @@
                 /**
                  * @type {any[]}
                  */
-                let index = []
+                let index = [];
 
                 if (checkKillable(element, move) != false) {
                     element = checkKillable(element, move);
@@ -194,22 +216,27 @@
                 }
 
                 if (element == null) {
-                    return
+                    return;
                 }
 
                 index = element.classList[1].split("-");
 
                 // check if the position is piece, if peace we stop the function
                 if (pieces[index[1]][index[2]] != 0) {
-                    return
+                    return;
                 }
                 // if the piece is the same as player piece we stop the function
-                if (player1Turn == true && oldElement.children[0].classList.contains("player-1")) {
-                    return
-                } else if (player1Turn == false && oldElement.children[0].classList.contains("player-2")) {
-                    return
+                if (
+                    player1Turn == true &&
+                    oldElement.children[0].classList.contains("player-1")
+                ) {
+                    return;
+                } else if (
+                    player1Turn == false &&
+                    oldElement.children[0].classList.contains("player-2")
+                ) {
+                    return;
                 }
-                
             }
 
             element.classList.add("possible-move");
@@ -217,9 +244,9 @@
             // change child class from hiddenMovable to movable
             element.children[0].classList.remove("hiddenMovable");
             element.children[0].classList.add("movable");
-            
-            // add move function
-            element.children[0].onclick = move;
+
+            // add move function to child element
+            element.children[0].addEventListener("click", movePiece);
 
             // display child element
             element.children[0].style.display = "block";
@@ -272,33 +299,33 @@
         // check for player one to go down only if their selected piece is not a king
         if (player1Turn == true && containPlayer1) {
             // calculate down left
-            addPossibleMove(elementGoDownLeft, 'goDownLeft');
+            addPossibleMove(elementGoDownLeft, "goDownLeft");
 
             // calculate down right
-            addPossibleMove(elementGoDownRight, 'goDownRight');
+            addPossibleMove(elementGoDownRight, "goDownRight");
 
             // if the selected piece is a king then we calculate the possible move from all direction
             if (event.target.classList.contains("king")) {
                 // calculate up left
-                addPossibleMove(elementGoUpLeft, 'goUpLeft');
+                addPossibleMove(elementGoUpLeft, "goUpLeft");
 
                 // calculate up right
-                addPossibleMove(elementGoUpRight, 'goUpRight');
+                addPossibleMove(elementGoUpRight, "goUpRight");
             }
         } else if (player1Turn == false && containPlayer2) {
             // calculate up left
-            addPossibleMove(elementGoUpLeft, 'goUpLeft');
+            addPossibleMove(elementGoUpLeft, "goUpLeft");
 
             // calculate up right
-            addPossibleMove(elementGoUpRight, 'goUpRight');
+            addPossibleMove(elementGoUpRight, "goUpRight");
 
             // if the selected piece is a king then we calculate the possible move from all direction
             if (event.target.classList.contains("king")) {
                 // calculate down left
-                addPossibleMove(elementGoDownLeft, 'goDownLeft');
+                addPossibleMove(elementGoDownLeft, "goDownLeft");
 
                 // calculate down right
-                addPossibleMove(elementGoDownRight, 'goDownRight');
+                addPossibleMove(elementGoDownRight, "goDownRight");
             }
         }
     }
@@ -308,38 +335,93 @@
      * @param {any} n
      */
     function findSelectedLocation(m, n) {
-
         // get their children
-        let element = document.querySelector(`.mn-${m}-${n}`)?.children[0]
+        let element = document.querySelector(`.mn-${m}-${n}`)?.children[0];
 
         // check if it's a selected piece
         if (element != null) {
             if (element.classList.contains("selected")) {
-                console.log("Found a piece at location " + m.toString() + ", " + n.toString())
-                return true
+                // console.log("Found a piece at location " + m.toString() + ", " + n.toString())
+                return true;
             }
         }
 
-        return false
+        return false;
+    }
+
+    /**
+     * @param {number} curRow
+     * @param {number} curCol
+     */
+    function checkKing(curRow, curCol) {
+        // check if the piece is a king
+        if (player1Turn) {
+            if (curRow == 7) {
+                pieces[curRow][curCol] = 3;
+                return true;
+            }
+        } else {
+            // console.log("player two " + curRow.toString() + ' ' + curCol.toString())
+            if (curRow == 0) {
+                pieces[curRow][curCol] = 4;
+                // console.log(pieces[curRow][curCol])
+                return true;
+            }
+        }
+
+        // console.log(pieces[curRow][curCol])
+        return false;
+    }
+
+    /**
+     * @param {number} currentRow
+     * @param {number} currentCol
+     * @param {number} minusRow
+     * @param {number} minusCol
+     */
+    function isKingMove(currentRow, currentCol, minusRow, minusCol) {
+        // check if king move
+        if (
+            pieces[currentRow + minusRow][currentCol + minusCol] == 3 &&
+            player1Turn
+        ) {
+            pieces[currentRow][currentCol] = 3;
+            return true;
+        } else if (
+            pieces[currentRow + minusRow][currentCol + minusCol] == 4 &&
+            !player1Turn
+        ) {
+            pieces[currentRow][currentCol] = 4;
+            return true;
+        }
+
+        let playerNumber = NaN;
+        // check which player turn it is
+        player1Turn ? (playerNumber = 1) : (playerNumber = 2);
+
+        // if not king move then check if it's a normal move
+        pieces[currentRow][currentCol] = playerNumber;
+
+        return false;
     }
 
     /**
      * @param {any} event
      */
-    function move(event) {
-        console.log("Move function called")
-
+    function movePiece(event) {
         /**
          * @type {string[]}
          */
-        let index = []
+        let index = [];
 
         // find index of parent element class that contain mn- and split it to get its position in the board
-        event.target.parentElement.classList.forEach((/** @type {string} */ className) => {
-            if (className.includes("mn-")) {
-                index = className.split("-")
+        event.target.parentElement.classList.forEach(
+            (/** @type {string} */ className) => {
+                if (className.includes("mn-")) {
+                    index = className.split("-");
+                }
             }
-        })
+        );
 
         // index[1] = current row, index[2] = current column
         let currentRow = parseInt(index[1]);
@@ -351,48 +433,122 @@
             findSelectedLocation(currentRow - 1, currentCol + 1),
             findSelectedLocation(currentRow + 1, currentCol - 1),
             findSelectedLocation(currentRow + 1, currentCol + 1),
-        ]
 
-        let playerNumber = NaN
-        // check which player turn it is
-        player1Turn ? playerNumber = 1 : playerNumber = 2
+            // start checking for kill move
+            findSelectedLocation(currentRow - 2, currentCol - 2),
+            findSelectedLocation(currentRow - 2, currentCol + 2),
+            findSelectedLocation(currentRow + 2, currentCol - 2),
+            findSelectedLocation(currentRow + 2, currentCol + 2),
+        ];
 
         for (let i = 0; i < findSelectedOrigin.length; i++) {
             if (findSelectedOrigin[i] == true) {
                 if (i == 0) {
-                    // change selected piece to movable space
-                    pieces[currentRow - 1][currentCol - 1] = 0
+                    // check if king move
+                    let isKing = isKingMove(currentRow, currentCol, -1, -1);
 
-                    // change movable space to selected piece
-                    pieces[currentRow][currentCol] = playerNumber
+                    // change selected piece to movable space
+                    pieces[currentRow - 1][currentCol - 1] = 0;
+
+                    if (isKing == false) {
+                        checkKing(currentRow, currentCol);
+                    }
                 } else if (i == 1) {
-                    // change selected piece to movable space
-                    pieces[currentRow - 1][currentCol + 1] = 0
+                    // check if king move
+                    let isKing = isKingMove(currentRow, currentCol, -1, 1);
 
-                    // change movable space to selected piece
-                    pieces[currentRow][currentCol] = playerNumber
+                    // change selected piece to movable space
+                    pieces[currentRow - 1][currentCol + 1] = 0;
+
+                    if (isKing == false) {
+                        checkKing(currentRow, currentCol);
+                    }
                 } else if (i == 2) {
-                    // change selected piece to movable space
-                    pieces[currentRow + 1][currentCol - 1] = 0
+                    // check if king move
+                    let isKing = isKingMove(currentRow, currentCol, 1, -1);
 
-                    // change movable space to selected piece
-                    pieces[currentRow][currentCol] = playerNumber
+                    // change selected piece to movable space
+                    pieces[currentRow + 1][currentCol - 1] = 0;
+
+                    if (isKing == false) {
+                        checkKing(currentRow, currentCol);
+                    }
                 } else if (i == 3) {
-                    // change selected piece to movable space
-                    pieces[currentRow + 1][currentCol + 1] = 0
+                    // check if king move
+                    let isKing = isKingMove(currentRow, currentCol, 1, 1);
 
-                    // change movable space to selected piece
-                    pieces[currentRow][currentCol] = playerNumber
+                    // change selected piece to movable space
+                    pieces[currentRow + 1][currentCol + 1] = 0;
+
+                    if (isKing == false) {
+                        checkKing(currentRow, currentCol);
+                    }
+                } else if (i == 4) {
+                    // check if king move
+                    let isKing = isKingMove(currentRow, currentCol, -2, -2);
+
+                    // change selected piece to movable space
+                    pieces[currentRow - 2][currentCol - 2] = 0;
+
+                    // remove the piece that was jumped over
+                    pieces[currentRow - 1][currentCol - 1] = 0;
+
+                    if (isKing == false) {
+                        checkKing(currentRow, currentCol);
+                    }
+                } else if (i == 5) {
+                    // check if king move
+                    let isKing = isKingMove(currentRow, currentCol, -2, 2);
+
+                    // change selected piece to movable space
+                    pieces[currentRow - 2][currentCol + 2] = 0;
+
+                    // remove the piece that was jumped over
+                    pieces[currentRow - 1][currentCol + 1] = 0;
+
+                    if (isKing == false) {
+                        checkKing(currentRow, currentCol);
+                    }
+                } else if (i == 6) {
+                    // check if king move
+                    let isKing = isKingMove(currentRow, currentCol, 2, -2);
+
+                    // change selected piece to movable space
+                    pieces[currentRow + 2][currentCol - 2] = 0;
+
+                    // remove the piece that was jumped over
+                    pieces[currentRow + 1][currentCol - 1] = 0;
+
+                    if (isKing == false) {
+                        checkKing(currentRow, currentCol);
+                    }
+                } else if (i == 7) {
+                    // check if king move
+                    let isKing = isKingMove(currentRow, currentCol, 2, 2);
+
+                    // change selected piece to movable space
+                    pieces[currentRow + 2][currentCol + 2] = 0;
+
+                    // remove the piece that was jumped over
+                    pieces[currentRow + 1][currentCol + 1] = 0;
+
+                    if (isKing == false) {
+                        checkKing(currentRow, currentCol);
+                    }
                 }
-                break
+                break;
             }
         }
-        clearPossibleMove()
-        switchPlayer()
-        
-        // check if the player has won
-    }
 
+        // clear possible move
+        clearPossibleMove();
+
+        // switch player turn
+        switchPlayer();
+
+        // check score
+        checkPlayerScore();
+    }
 </script>
 <!-- Script end here -->
 
@@ -431,6 +587,7 @@
 <!-- HTML end here -->
 
 
+
 <!-- Style goes here  -->
 <style>
     .container {
@@ -440,14 +597,14 @@
         display: grid;
         place-items: center;
     }
-    
+
     .board {
         display: grid;
         grid-template-columns: repeat(10, 5rem);
         border-radius: 0.5rem solid white;
         box-shadow: 0 0 10px 5px #ffffff;
     }
-    
+
     .cell {
         width: 5rem;
         height: 5rem;
@@ -459,30 +616,34 @@
     .dark {
         background-color: #251e21;
     }
-    
+
     .pieces {
         width: 4rem;
         height: 4rem;
         border-radius: 100%;
         border: none;
     }
-    
+
     .player-1 {
         background-color: rgb(164, 164, 192);
     }
-    
+
     .player-2 {
         background-color: rgb(248, 147, 129);
     }
-    
+
     .selected {
         box-shadow: 0 0 10px 5px #ffffff;
     }
-    
+
+    .king {
+        box-shadow: 0 0 10px 5px #3cff00;
+    }
+
     span {
         font-size: 2rem;
         font-weight: 700;
         margin-bottom: 1rem;
     }
-    </style>
+</style>
 <!-- Style end here  -->
